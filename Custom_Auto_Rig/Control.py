55c9bf866067811d0616ecdba3cc47e80b41e034
/JAlexMc <jonathan4812@gmail.com>
2024-05-03 18:50:36 -0700

    start control modification script

---
+++ Custom_Auto_Rig/Control.py
@@ -0,0 +1,3 @@
+# create a method to extract the distance from OG location from the control objects
+# create a method to iterate through the dictionary to give the distance to the control objects
+# figure out how to deal with rotation and scale.
